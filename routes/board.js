9e2a910fdfe99523ab1935c209e37131e025fbdd
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-11-06 21:04:23 -0600

    Make choose.pug and choose.js

--- routes/board.js
+++ routes/board.js
@@ -1,22 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  const query = req.query;
-  const rows = Number(query.rows);
-  const cols = Number(query.cols);
-
-  if (
-    Number.isInteger(rows) &&
-    Number.isInteger(cols) &&
-    rows >= 1 && rows <= 10 &&
-    cols >= 1 && cols <= 10
-  ) {
-    res.render('board', { title: 'Board Display', query, rows, cols });
-  } else {
-    res.status(400).send('Invalid input');
-  }
-});
-
+router.get('/', function(req, res, next) {
+  res.render('board', { title: 'Board Display' });
+  let query = req.query
+  console.log(`rows ${query.rows}`)
+  console.log(`cols ${query.cols}`)
+  res.render('board', {title: 'Board Display', query : query})
+});  
 module.exports = router;
