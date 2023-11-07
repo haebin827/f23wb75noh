01199b59e86f295c20b65fa60572d4958dac61e9
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-11-06 18:28:50 -0600

    Add query strings

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var electionsRouter = require('./routes/elections');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use('/elections', electionsRouter);
+app.use('/board', boardRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
