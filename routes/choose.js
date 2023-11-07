6ef4c813d4495fcbec150b933549265f61f93150
haebin827 <99354243+haebin827@users.noreply.github.com>
2023-11-06 21:28:27 -0600

    Bonus

--- routes/choose.js
+++ routes/choose.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Choose' });
+  res.render('choose', { title: 'Choose' });
 });
 
 module.exports = router;
